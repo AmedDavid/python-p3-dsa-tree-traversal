9cdd68152e248536dfd4bf7cbc878fe3d7344535
David Amedi <amukoahdavid@gmail.com>
2025-05-25 00:36:11 +0300

    Implemented get_element_by_id with depth-first traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,30 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if not self.root:
+            return None
+        
+        def depth_first(node):
+            if node['id'] == id:
+                return node
+            for child in node['children']:
+                result = depth_first(child)
+                if result:
+                    return result
+            return None
+
+        return depth_first(self.root)
+
+        # Alternative: Breadth-first traversal
+        """
+        from collections import deque
+        nodes_to_visit = deque([self.root])
+        while nodes_to_visit:
+            node = nodes_to_visit.popleft()
+            if node['id'] == id:
+                return node
+            nodes_to_visit.extend(node['children'])
+        return None
+        """
